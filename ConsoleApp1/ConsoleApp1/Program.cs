61fe325a90fc9dbfd5881785d85c3cefd347a1e3
KrisjanisAboltins <krisjanis.aboltins@inbox.lv>
2022-05-18 22:11:29 +0300

    test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,20 @@
 ﻿
+// 
+
 
 
 Console.WriteLine("Labvakar");
+Console.WriteLine("Šī ir skaitļu minēšanas spēle");
 
+Console.WriteLine("Lūdzu, ieraksti skaitli no 1 līdz 50");
+
+string skaitlis1 = Console.ReadLine();
+
+string skaitlis1Text = Console.ReadLine();
+int skaitlis11Number = int.Parse(skaitlis1Text);
+
+Console.WriteLine($"Tu ierakstīji skaitli {skaitlis11Number}, skaitlis nav pareizs mini vēlreiz");
+
+Console.WriteLine($"Tu ierakstīji skaitli {skaitlis11Number}, skaitlis ir pareizs, TU UZMINĒJI :)");
+
+Console.WriteLine($"Vai Tu vēlies pāriet uz nākamo līmeni");
