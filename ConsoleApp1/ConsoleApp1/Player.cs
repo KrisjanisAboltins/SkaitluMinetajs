948eb7d6e4220d70adc945ab2c2508c4a93f8758
Sandra Eglīte <san.eglite@gmail.com>
2022-05-22 13:56:06 +0300

    izveidoju Player klasi un piedefinēju raksturlielumus

---
+++ ConsoleApp1/ConsoleApp1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Player
+    {
+        public string Name;     
+        public int Age { get; set; }    
+        public int Attempts { get; set; }   
+
+        public int Points { get; set; } 
+
+        public int Wins { get; set; }   
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,33 @@
 ﻿
+//piedāvāju, ka izveidojam klasi Player, kur definējam visus spēlētāju raksturlielumus
+//(šobrīd ieliku vārdu, vecumu, mēģinājumu skaitu, iegūtos punktus (ja izlemsim tos skaitīt)
+//un uzvaras). In tad uzreiz katra spēlētāja info saglabāsim un varēsim izmantot.
+//Ja pareizi saprotu, programmas failā tas izskatītos apmēram šādi:
+using ConsoleApp1
+    { 
+
+    Player user1 = new Player();
+    user1.Name = "";
+    user1.Age = 0; // ievades dati laikam jāpārbauda, lai int tiešām ir int nevis string
+    user1.Attempts = 0; //šie nākamie trīs iegūsies spēlēs laikā un mums tos vajadzēs kaut kā uzkrāt
+    user1.Points = 0;
+    user1.Wins = 0;
+
+    Player user2 = new Player();
+    user2.Name = "";
+    user2.Age = 0;
+    user2.Attempts = 0;
+    user2.Points = 0;
+    user2.Wins = 0;
+    // utt. varam definēt spēlētāju skaitu vai jāizdomā, vai var būt neierobežots.
+    // Saprotu, ka ja gribam vairākus spēlētājus un pēc tam arī visu ko darīt ar šiem
+    //datiem, tad Krišjāņa kodā jālieto piemēram, 34 rindā nevis userName, bet user1.
+    // šis jau sāk palikt sarežģītāk, jo kodā uzreiz arī jāpiedefinē mēģinājumu skaits
+    //pirmajam spēlētājam tas būtu user1.Attempts, utt.
+
+
+}
+
 
 
 Console.WriteLine("Labdien,");
